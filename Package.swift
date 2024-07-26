008c6b1068ebaebc52d6651cefe07633f76b569b
Sheel Patel <sheel@doorstep.ai>
2024-07-26 13:39:47 -0400

    initial

--- Package.swift
+++ Package.swift
@@ -5,17 +5,28 @@ import PackageDescription
 
 let package = Package(
     name: "DoorstepAIiOSWrapper",
+    platforms: [
+        .iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DoorstepAIiOSWrapper",
             targets: ["DoorstepAIiOSWrapper"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/ikhvorost/ReactBridge.git", from: "1.0.0"),
+        .package(url: "https://github.com/sheelpatel0118/DoorstepAIiOSPackage.git", branch: "main")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DoorstepAIiOSWrapper"),
+            name: "DoorstepAIiOSWrapper",
+            dependencies: [
+                "ReactBridge",
+                "DoorstepAIiOSPackage"
+            ]
+        ),
         .testTarget(
             name: "DoorstepAIiOSWrapperTests",
             dependencies: ["DoorstepAIiOSWrapper"]),
--- Sources/DoorstepAIiOSWrapper/DoorstepAIiOSWrapper.swift
+++ Sources/DoorstepAIiOSWrapper/DoorstepAIiOSWrapper.swift
@@ -1,2 +1,17 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import React
+import ReactBridge
+//import DoorstepAIiOSSDK
+
+@ReactModule(jsName: "Calendar")
+class CalendarModule: NSObject, RCTBridgeModule {
+    
+    @ReactMethod
+    @objc func test() {
+        print("testing the method")
+    }
+    
+}
+
+//import DoorstepAIiOSSDKFramework
